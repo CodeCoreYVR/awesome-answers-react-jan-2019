448c29ab7ab4f604899f56223dc6f6426cb7cc09
Steve Godin <steve.god@gmail.com>
2019-03-15 15:35:42 -0700

    Add demo for Styling Your Components

---
+++ src/components/AnswerDetails.js
@@ -0,0 +1,30 @@
+// const React = require("react")
+import React from "react";
+// The "import" is the native JavaScript way to import modules.
+// Here, we use to import our "react" package.
+
+// Create React App setups a project where importing CSS
+// is possible. Doing so will automatically the CSS
+// file a <style> tag appended to the <head>.
+import "../styles/AnswerDetails.css";
+
+// This function is a React Component
+// It is a function that returns a React Element
+function AnswerDetails(props) {
+  return (
+    // Returns a React Element
+    <div className="AnswerDetails">
+      <p>
+        {props.body}
+        <br />
+        By {props.author.full_name}
+      </p>
+      <p>Answered at {props.created_at.toLocaleString()}</p>
+    </div>
+  );
+}
+
+// module.exports = AnswerDetails
+// To be able to import code from this file into another file, we use
+// the "export" keyword instead of "module.exports"
+export default AnswerDetails;
