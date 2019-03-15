9b52af63d23fcd4072b29fd88301bc5ae6a152f1
Steve Godin <steve.god@gmail.com>
2019-03-15 16:00:11 -0700

    Add demo for Question Index Page

--- src/index.js
+++ src/index.js
@@ -2,9 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import * as serviceWorker from "./serviceWorker";
-import QuestionShowPage from "./components/QuestionShowPage";
+import App from "./components/App";
 
-ReactDOM.render(<QuestionShowPage />, document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
