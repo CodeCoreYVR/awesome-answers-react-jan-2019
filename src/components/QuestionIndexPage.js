4f9a487a9c52f87bc62fd83c53e1a0e2e9d872e0
Maxwell Gordon <maxwellgordon@Maxwells-MacBook-Pro.local>
2019-03-18 13:16:46 -0700

    Add solution to Question Index goes to Class

--- src/components/QuestionIndexPage.js
+++ src/components/QuestionIndexPage.js
@@ -1,27 +1,34 @@
-import React from "react";
-import questionData from "../questionData";
+import React, { Component } from 'react';
+import questionData from '../questionData';
 
-function QuestionIndexPage() {
-  return (
-    <main>
-      <h1>Questions</h1>
-      <ul>
-        {questionData.map(question => (
-          <li key={question.id}>
-            <p>
-              {question.title}
-              <br />
-              <small>Seen {question.view_count} time(s)</small>
-              {" – "}
-              <small>
-                Created at {new Date(question.created_at).toLocaleString()}
-              </small>
-            </p>
-          </li>
-        ))}
-      </ul>
-    </main>
-  );
+class QuestionIndexPage extends Component {
+	render() {
+		return (
+			<main>
+				<h1>Questions</h1>
+				<ul>
+					{questionData.map((question) => (
+						<li key={question.id}>
+							<p>
+								{question.title}
+								<br />
+								<small>
+									Seen {question.view_count} time(s)
+								</small>
+								{' – '}
+								<small>
+									Created at{' '}
+									{new Date(
+										question.created_at,
+									).toLocaleString()}
+								</small>
+							</p>
+						</li>
+					))}
+				</ul>
+			</main>
+		);
+	}
 }
 
 export default QuestionIndexPage;
